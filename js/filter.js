41b0eb63c61d05d870febf7ff1b9b228909d636f
burnaa <burantsetseg175@gmail.com>
2024-10-29 15:02:17 +0800

    jss

---
+++ js/filter.js
@@ -0,0 +1,25 @@
+class Animal{
+    constructor(id,ner,turul, nas, huis,dugaar){
+        this.id=id;
+        this.name=ner;
+        this.type=turul;
+        this.age=nas;
+        this.sex=huis;
+        this.number=dugaar;
+    }
+    searchType(turul){
+        if(this.type==turul){
+
+        }
+    }
+    searchSex(huis){
+        if(this.sex==huis){
+
+        }
+    }
+    searchAge(){
+        if(this.age=nas){
+            
+        }
+    }
+}
