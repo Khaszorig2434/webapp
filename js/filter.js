e05de2bb5deff252441f5597fd341bb0bd3554a8
burnaa <burantsetseg175@gmail.com>
2024-11-12 15:26:46 +0800

    hfvk

--- js/filter.js
+++ js/filter.js
@@ -31,7 +31,7 @@ class list{
         answer = []
         for(let i = 0; i < this.amidtad.length; i++){
             Unen = true;
-            for(int j = 0; j < yalgah.length; j++){
+            for(let j = 0; j < yalgah.length; j++){
                 Unen = Unen & yalgah[j](amitad[i]);
             }
             if(Unen) answer.push(amitad[i]);
@@ -41,7 +41,7 @@ class list{
 }
 
 jagsaalt = new list();
-shalga1(amitan){
+const shalga1(amitan){
     return amitan.name == 'Muur';
 }
 shalga2(amitan){
