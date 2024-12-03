c0e2840ef4ffe79a190db47ba58a66f4fb76a589
Khaszorig2434 <159092996+Khaszorig2434@users.noreply.github.com>
2024-12-03 11:28:28 +0800

    product dynamically rendered issue in cart button

--- js/sagsniih/product.js
+++ js/sagsniih/product.js
@@ -1,71 +1,90 @@
 export default class Product {
-    constructor(prod) {     
-        this.id = prod.id;
-        this.name = prod.ner;
-        this.type = prod.turul;
-        this.size = [prod.one, prod.two, prod.three];
-        this.color = [prod.oneColor, prod.twoColor, prod.threeColor];
-        this.origin = [prod.exp, prod.imp];
-        this.age = [prod.ehleh, prod.duusah];
-        this.gpa=[prop.gpa];
-        this.price = prod.price;
+    constructor(prod) {
+        this.id = prod?.id;
+        this.name = prod.article.title;
+        this.type = prod.article.category;
+        this.size = prod.article.options.sizes.map(size => size.label);
+        this.color = prod.article.options.colors.map(color => color.label);
+        this.origin = prod.article.details.origin;
+        this.certification = prod.article.details.certification;
+        this.mainpic = prod.mainImage.src;
+        this.subpic = prod.thumbnails.map(thumbnail => thumbnail.src);
+        this.category = prod.article.category;
+        this.age = [prod.article.details.manufactureDate, prod.article.details.expiryDate];
+        this.price = {
+            currency: prod.article.price.currency,
+            amount: prod.article.price.amount
+        };
     }
-    render() { 
+    render() {
         return `<div class="container">
-        <ul class="jagsaah">
-            <li><img src="images/oneprob/neg.png" alt="Зураг 1"></li>
-            <li><img src="images/oneprob/hoyr.png" alt="Зураг 2"></li>
-            <li><img src="images/oneprob/gurv.png" alt="Зураг 3"></li>
-            <li><img src="images/oneprob/duruv.png" alt="Зураг 4"></li>
-        </ul>
-        <img class="big" src="images/oneprob/neg.png" alt="Бүтээгдэхүүний нүүр">
-        <article>
-            <h2>${this.name}</h2>
-            <pre>${this.type}   (${this.id})</pre>
-            <aside class="songolt">
-                <pre>Хэмжээ</pre>
-                <form>
-                    ${this.size.map((s, i) => `
-                        <input type="radio" id="size${i}" name="choice_size">
-                        <label for="size${i}">${s}</label>
-                    `).join('')}
-                </form>
-            </aside>
-            <aside>
-                <pre>Өнгө</pre>
-                <form>
-                    ${this.color.map((c, i) => `
-                        <input type="radio" id="color${i}" name="choice_color">
-                        <label for="color${i}">${c}</label>
-                    `).join('')}
-                </form>
-            </aside>
-            <p>Бүтээгдэхүүний гарал: ${this.origin}</p>
-            <p>Үйлдвэрлэгдсэн огноо: ${this.age[0]}</p>
-            <p>Дуусах огноо:${this.age[1]}</p>
-            <p>Баталгаажуулалт:${this.gpa}</p>
-
-            <span>₮</span><span>${this.price}</span>
-            <button id="add-to-cart">🛍️Сагсанд нэмэх🛒</button>
-        </article>
-    </div>`;
+            <ul class="jagsaah">
+                <li id="neg"><img src="${this.subpic[0]}" alt="Thumbnail 1"></li>
+                <li id="xoyr"><img src="${this.subpic[1]}" alt="Thumbnail 2"></li>
+                <li id="gurav"><img src="${this.subpic[2]}" alt="Thumbnail 3"></li>
+                <li id="duruv"><img src="${this.subpic[3]}" alt="Thumbnail 4"></li>
+            </ul>
+            <div id="mainpicture">
+                <img class="big" src="${this.mainpic}" alt="Main Product Image">
+            </div>
+            <article>
+                <h2 id="title">${this.name}</h2>
+                <pre>${this.type}</pre>
+                <aside class="songolt">
+                    <pre>Хэмжээ</pre>
+                    <form>
+                        ${this.size.map((size, index) => `
+                            <input type="radio" name="choice_size" id="size${index + 1}">
+                            <label for="size${index + 1}">${size}</label>
+                        `).join('')}
+                    </form>
+                </aside>
+                <aside>
+                    <pre>Өнгө</pre>
+                    <form>
+                        ${this.color.map((color, index) => `
+                            <input type="radio" name="choice_color" id="color${index + 1}">
+                            <label for="color${index + 1}">${color}</label>
+                        `).join('')}
+                    </form>
+                </aside>
+                <p id="uildverlesen">Үйлдвэрлэгдсэн огноо: ${this.age[0]}</p>
+                <p id="expire">Дуусах огноо: ${this.age[1]}</p>
+                <p id="origin">Бүтээгдэхүүний гарал: ${this.origin}</p>
+                <p id="standart">Баталгаажуулалт: ${this.certification}</p>
+                <span id="currency">${this.price.currency}</span><span id="total">${this.price.amount}</span>
+                <br>
+                <button id="pay-button">🛒Сагсанд нэмэх🛒</button>
+            </article>
+        </div>`;
     }
 
-        // Энд 'add-to-cart' товчлуур дээр сонголтуудыг авах үйлдлийг хийж байна
-        setupAddToCartButton() {
-        document.getElementById('add-to-cart').addEventListener('click', function() {
+    // Энд 'add-to-cart' товчлуур дээр сонголтуудыг авах үйлдлийг хийж байна
+    setupAddToCartButton() {
+        document.getElementById('pay-button').addEventListener('click', function () {
+            console.log("Clicked.");
             // Зөвхөн сонгосон утгуудыг авах
-            const size = document.querySelector('input[name="choice_size"]:checked').value;
-            const color = document.querySelector('input[name="choice_color"]:checked').value;
-    
+            const sizeElement = document.querySelector('input[name="choice_size"]:checked');
+            if (!sizeElement) {
+                alert("Хэмжээг сонгоно уу.");
+                return;
+            }
+            const size = sizeElement.value;
+            const colorElement = document.querySelector('input[name="choice_color"]:checked');
+            if (!colorElement) {
+                alert("Өнгөө сонгоно уу.");
+                return;
+            }
+            const color = colorElement.value;
+
             // app.cart.addProduct болон app.refreshCart функцуудыг дуудна
             app.cart.addProduct(this.id, size, color);
             app.refreshCart();
         });
-        }
-    
+    }
 
-    renderCompact(){
+
+    renderCompact() {
         return `<section class="product">
             <aside id="egnee">
                 <h3>${this.name}</h3>
