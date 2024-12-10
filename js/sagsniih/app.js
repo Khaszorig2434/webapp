ec8ee9d6ba92959349387eb882f9622dbf98343a
Khaszorig2434 <159092996+Khaszorig2434@users.noreply.github.com>
2024-12-10 09:57:40 +0800

    fixing oneProduct "again"

--- js/sagsniih/app.js
+++ js/sagsniih/app.js
@@ -5,10 +5,19 @@ import Data from "./data.js";
 
 const sags = new Cart();
 app.cart = sags;
-
-const data = new Data("https://dummyjson.com/products");
+const data = new Data("https://api.jsonbin.io/v3/b/674e28d9e41b4d34e45e9fbe");
 app.products = await data.refreshData();
 
+console.log("Fetched products:", app.products);
+
+let productsHTML = "";
+app.products.forEach((productData) => {
+    const product = new Product(productData);
+    productsHTML += product.render();
+    setTimeout(() => product.setupAddToCartButton(), 0);
+});
+
+document.getElementById("prodSection").innerHTML=productsHTML;
 if(sagsandBaigaaBaraaniiToo>0)
     document.getElementById("cart").innerHTML= sags.render();
 
