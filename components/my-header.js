f0e0ce90ac7d3b1751ec74980948fa870faeb54a
burnaa <burantsetseg175@gmail.com>
2024-12-28 23:09:26 +0800

    login html+css

--- components/my-header.js
+++ components/my-header.js
@@ -1,4 +1,5 @@
 import "./my-cart.js";
+import "./my-login.js";
 class MyHeader extends HTMLElement {
 
     constructor() {
@@ -12,15 +13,7 @@ class MyHeader extends HTMLElement {
         <img id="logo" src="assets/images/clean_white_logo.png" alt="Компаний лого" width="80" onclick="window.location.href='index.html'">
         <input type="search" name="search" id="haih" placeholder="Бүтээгдэхүүн хайх ">
         <aside>
-            <button aria-label="хэрэглэгч хэсэг" onclick="window.location.href='user.html'">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="lucide lucide-user">
-                    <title>Хэрэглэгчийн дүрс</title>
-                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                    <circle cx="12" cy="7" r="4" />
-                </svg>
-            </button>
+            <my-login></my-login>
             <my-cart id="my-cart"></my-cart>
         </aside>
     </header>
